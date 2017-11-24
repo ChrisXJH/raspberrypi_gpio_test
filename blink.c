5f5b9089bde1f0387c06ef75f8fbf3a165903d98
ChrisXJH <gzchrisxjh@gmail.com>
2017-11-23 20:05:05 -0500

    Edited blink.c

--- blink.c
+++ blink.c
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[]) {
     wiringPiSetupGpio();
     pinMode(LED, OUTPUT);
 
-    for (size_t i = 0; i < 10; i++) {
+    while (1) {
         digitalWrite(LED, HIGH);
         delay(500);
         digitalWrite(LED, LOW);
