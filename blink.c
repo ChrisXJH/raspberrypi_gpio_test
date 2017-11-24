5125ff87abb3840cb4c66cee724a0116de309919
ChrisXJH <gzchrisxjh@gmail.com>
2017-11-23 20:02:20 -0500

    Edited blink.c

--- blink.c
+++ blink.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include "wiringPi.h"
 
+#define LED 5;
 
 int main(int argc, char const *argv[]) {
+    wiringPiSetupGpio();
+    pinMode(LED, OUTPUT);
+
     for (size_t i = 0; i < 10; i++) {
+        digitalWrite(LED, HIGH);
         delay(500);
-        printf("s\n");
+        digitalWrite(LED, LOW);
+        delay(500);
     }
     return 0;
 }
